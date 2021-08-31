181def40b3a68b41a4bf6036336f0284c8a9f0cd
Thomas Brunström <choice@warzone.se>
2021-08-31 15:37:33 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # academy-proxy-server
+
+## TO INSTALL:
+
+Run: `npm install` in the terminal.
+
+## TO START:
+
+Run `npm run start` in the terminal
---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+const fetch = require('node-fetch');
+
+const app = express();
+const PORT = 5050;
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/xkcd/:id', (req, res) => {
+  fetch(`http://xkcd.com/${req.params.id}/info.0.json`)
+    .then(result => result.json())
+    .then(result => res.json(result))
+    .catch(err => res.status(500).send());
+});
+
+app.get('/xkcd', (req, res) => {
+  fetch('http://xkcd.com/info.0.json')
+    .then(result => result.json())
+    .then(result => res.json(result));
+});
+
+app.post('/reverse', (req, res) => {
+  let { message } = req.body;
+  if (!message) {
+    message = 'Im missing the message item from the form'.split('').reverse().join('');
+  }
+  res.json({
+    message: message.split("").reverse().join("")
+  });
+});
+
+app.post('/echo', (req, res) => {
+  res.json(req.body);
+})
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`)
+})
