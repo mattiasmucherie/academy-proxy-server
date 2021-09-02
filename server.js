c573e1933f9889d36c01a553ef29908ae8de6d73
Thomas Brunström <choice@warzone.se>
2021-09-02 16:08:49 +0200

    Updates to server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-const fetch = require('node-fetch');
+const express = require("express");
+const cors = require("cors");
+const fetch = require("node-fetch");
 
 const app = express();
 const PORT = 5050;
@@ -9,33 +9,37 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/xkcd/:id', (req, res) => {
+app.get("/xkcd/:id", (req, res) => {
   fetch(`http://xkcd.com/${req.params.id}/info.0.json`)
-    .then(result => result.json())
-    .then(result => res.json(result))
-    .catch(err => res.status(500).send());
+    .then((result) => result.json())
+    .then((result) => res.json(result))
+    .catch((err) => res.status(500).send());
 });
 
-app.get('/xkcd', (req, res) => {
-  fetch('http://xkcd.com/info.0.json')
-    .then(result => result.json())
-    .then(result => res.json(result));
+app.get("/xkcd", (req, res) => {
+  fetch("http://xkcd.com/info.0.json")
+    .then((result) => result.json())
+    .then((result) => res.json(result));
 });
 
-app.post('/reverse', (req, res) => {
+app.post("/reverse", (req, res) => {
   let { message } = req.body;
   if (!message) {
-    message = 'Im missing the message item from the form'.split('').reverse().join('');
+    message = "Im missing the message item from the form"
+      .split("")
+      .reverse()
+      .join("");
   }
   res.json({
-    message: message.split("").reverse().join("")
+    message: message.split("").reverse().join(""),
+    comicId: 32,
   });
 });
 
-app.post('/echo', (req, res) => {
+app.post("/echo", (req, res) => {
   res.json(req.body);
-})
+});
 
 app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`)
-})
+  console.log(`listening on port ${PORT}`);
+});
